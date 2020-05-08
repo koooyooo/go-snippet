f12618f130d190c4577bb4307ff75cee5efa0a21
koooyooo <jab.cross.hook@gmail.com>
2020-05-09 04:10:24 +0900

    enhance gin test

--- gin_http/gin_test.go
+++ gin_http/gin_test.go
@@ -3,6 +3,7 @@ package gin_http
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
@@ -10,13 +11,32 @@ import (
 func TestGin(t *testing.T) {
 	// Prepare test target
 	target := func(c *gin.Context) {
+		c.Writer.WriteHeader(http.StatusOK)
 		c.Writer.WriteString("Hello Gin")
 	}
-
 	// Create New Context with Recorder
 	w := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(w)
 	target(c)
 
+	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, "Hello Gin", w.Body.String())
 }
+
+func TestGinJSON(t *testing.T) {
+	// Prepare test target
+	target := func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "Gin",
+		})
+	}
+	// Create New Context with Recorder
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	target(c)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, `{"Hello":"Gin"}`, w.Body.String())
+}
+
+
