2a6ab2df91801d8f2bc6356b6774c3b2bc3c180b
koooyooo <jab.cross.hook@gmail.com>
2020-05-09 04:47:52 +0900

    add gin server test

--- gin_http/gin_test.go
+++ gin_http/gin_test.go
@@ -3,16 +3,20 @@ package gin_http
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestGin(t *testing.T) {
+func TestGinFuncOfCtxWriter(t *testing.T) {
 	// Prepare test target
 	target := func(c *gin.Context) {
 		c.Writer.WriteHeader(http.StatusOK)
-		c.Writer.WriteString("Hello Gin")
+		_, err := c.Writer.WriteString("Hello Gin")
+		if err != nil {
+			panic(err)
+		}
 	}
 	// Create New Context with Recorder
 	w := httptest.NewRecorder()
@@ -23,7 +27,7 @@ func TestGin(t *testing.T) {
 	assert.Equal(t, "Hello Gin", w.Body.String())
 }
 
-func TestGinJSON(t *testing.T) {
+func TestGinFuncOfCtxJSON(t *testing.T) {
 	// Prepare test target
 	target := func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -39,4 +43,28 @@ func TestGinJSON(t *testing.T) {
 	assert.Equal(t, `{"Hello":"Gin"}`, w.Body.String())
 }
 
+func TestDo(t *testing.T) {
+	gin.SetMode(gin.TestMode)
 
+	router := gin.Default()
+	router.Any("/path", func(c *gin.Context) {
+		c.Writer.WriteHeader(http.StatusOK)
+		c.Writer.WriteString("Hello")
+	})
+	// Don't call Run server this time
+	// router.Run(":8080")
+
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	req, err := http.NewRequest("GET", ts.URL + "/path", nil)
+	assert.Nil(t, err)
+
+	resp, err := http.DefaultClient.Do(req)
+	assert.Nil(t, err)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello", string(bodyBytes))
+}
